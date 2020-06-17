f665b40e78a2aec642b61e364c54187578192a54
Katiussa da Silveira Pereira <katiussa.pereira@ntconsult.com.br>
2020-06-17 20:55:27 -0300

    mongo com docker

---
+++ GuestModel.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteMongo
+{
+    public class GuestModel
+    {
+        [BsonId]  // _id
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+
+        [BsonElement("Name")]
+        public string FullName { get; set; }
+        public bool HasConfirmed { get; set; }
+    }
+}
